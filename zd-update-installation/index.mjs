6ea55c29c3aff2e7021a82019bd60b5c0b6bd78a
Andrej Grobler <andrej.grobler@voicefoundry.com.au>
2024-10-17 17:21:27 +1100

    Implemented setting of secure settings at installation

--- zd-update-installation/index.mjs
+++ zd-update-installation/index.mjs
@@ -5,30 +5,37 @@ import { init } from "./zdApi.mjs";
 import { getApiKeyValue } from "./apiGateway.mjs";
 
 export const handler = async (event, context) => {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+    // console.log('Received event:', JSON.stringify(event, null, 2));
 
     let apiKeyValue = "";
-    let apiEndpoint = "";
+    let apiSubdomain = "";
+    let apiEnv = "";
     let installed = "";
 
     const request = event.RequestType;
 
-    if (request === "Create" || request === "Update") {
+    if (request === "Create" || request === "Update" || request === "Rotate") {
         const keyId = process.env.STATUS_SYNC_API_KEY;
         apiKeyValue = await getApiKeyValue(keyId);
-        apiEndpoint = process.env.STATUS_SYNC_API_URL;
+        const url = process.env.STATUS_SYNC_API_URL;
+        apiSubdomain = url.slice(8, url.indexOf(".amazonaws.com"));
+        apiEnv = url.substring(8).split("/")[1];
         installed = "installed";
     }
 
     const axiosClient = init("api/v2/apps/installations");
     if (axiosClient) {
         await updateInstallation(axiosClient, {
-            key: "gwAPIKeyDev",
+            key: "z2cStatusApiKey",
             value: apiKeyValue,
         });
         await updateInstallation(axiosClient, {
-            key: "gwAPIUrlDev",
-            value: apiEndpoint,
+            key: "z2cStatusSubdomain",
+            value: apiSubdomain,
+        });
+        await updateInstallation(axiosClient, {
+            key: "z2cStatusEnv",
+            value: apiEnv,
         });
         await updateInstallation(axiosClient, {
             key: "zendesk2ConnectStatus",
@@ -36,10 +43,13 @@ export const handler = async (event, context) => {
         });
     }
 
-    await response.send(event, context, response.SUCCESS, {}).catch((error) => {
-        console.error("Error sending cfn response: ", error);
-    });
+    if (request !== "Rotate") {
+        await response.send(event, context, response.SUCCESS, {}).catch((error) => {
+            console.error("Error sending cfn response: ", error);
+        });
+    }
 };
+
 
 const updateInstallation = async (webClient, setting) => {
     return webClient.put(`/${process.env.ZD_INSTALLATION_ID}`, {
